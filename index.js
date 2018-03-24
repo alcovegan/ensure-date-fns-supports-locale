6311ed8cb4d6bf5d4b76bfc6873974f807da5d99
Aleksandr Sh <alcovegan@gmail.com>
2018-03-24 22:15:49 +0300

    fix: fixed bug-typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const supportedLocales = require('./supported-locales');
 
 function ensure(locales) {
     if(Array.isArray(locales)) {
-        return supportedLocales.every(locale => supportedLocales.indexOf(locale) !== -1)
+        return locales.every(locale => supportedLocales.indexOf(locale) !== -1)
     }
     return supportedLocales.indexOf(locales) !== -1
 }
