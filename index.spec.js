a5b17822bf562cef215cb76cb22a071ba8e8b53e
Aleksandr Sh <alcovegan@gmail.com>
2018-03-24 22:15:09 +0300

    test: add falsy test cases

--- index.spec.js
+++ index.spec.js
@@ -10,10 +10,14 @@ test('lib working correctly when passed single locale', () => {
     expect(lib('eo')).toBe(true);
     expect(lib('nb')).toBe(true);
     expect(lib('zh_tw')).toBe(true);
+    expect(lib('zzz')).toBe(false);
+    expect(lib('af')).toBe(false);
 })
 
 test('lib working correctly when passed array of locales', () => {
     expect(lib(['ru', 'en', 'eo'])).toBe(true);
     expect(lib(['nb', 'zh_tw'])).toBe(true);
     expect(lib(['ko'])).toBe(true);
+    expect(lib(['zzz', 'en', 'ru'])).toBe(false);
+    expect(lib(['zzz'])).toBe(false);
 })
