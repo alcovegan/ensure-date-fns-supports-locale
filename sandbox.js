091149b7e7b1a0d1179df259ecd2d3f932804db7
Aleksandr Sh <alcovegan@gmail.com>
2018-03-24 22:21:55 +0300

    chore: added sandbox file

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+const ensureLocale = require('./');
+
+// single string
+const ensureIsRussianSupported = ensureLocale('ru'); // true
+const ensureIsElvenSupported = ensureLocale('elv'); // false
+
+// array
+const ensureLocalesSupported = ensureLocale(['ru', 'en', 'eo']); // true
+const ensureLocalesSupportedOneMoreTime = ensureLocale(['zzz', 'en', 'eo']); // false
+
+console.log(
+    ensureIsRussianSupported,
+    ensureIsElvenSupported
+)
+
+console.log(
+    ensureLocalesSupported,
+    ensureLocalesSupportedOneMoreTime
+)
